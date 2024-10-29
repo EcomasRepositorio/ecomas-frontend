b752c02fe9a54c3aad4a86c5c0fd8d2eb0bf7366
zunniga <jmoficial2003@gmail.com>
2024-10-29 10:25:43 -0500

    fixed the code number 10 caracters

--- components/home/SwiperCursosDestacados.jsx
+++ components/home/SwiperCursosDestacados.jsx
@@ -174,8 +174,8 @@ const SwiperCursosDestacados = () => {
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -193,8 +193,8 @@ const SwiperCursosDestacados = () => {
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
--- components/home/TestimoniosFb.tsx
+++ components/home/TestimoniosFb.tsx
@@ -187,8 +187,8 @@ const TestimoniosFb = () => {
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -206,8 +206,8 @@ const TestimoniosFb = () => {
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
