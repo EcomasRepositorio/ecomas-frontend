946030203bc4f5734cc3345d2c9f0915426e5f3a
zunniga <jmoficial2003@gmail.com>
2025-01-23 09:32:10 -0500

    fixed error in the name of graduate alimentary

--- components/diplomadosComponents/diplomadosNav.tsx
+++ components/diplomadosComponents/diplomadosNav.tsx
@@ -13,41 +13,56 @@ const Graduate = () => {
   return (
     <section id="/diplomados">
       <div className=" p-5 flex flex-col">
-        <div id='civil' className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72">
-            DIPLOMADOS EN INGENIERÍA CIVIL
+        <div
+          id="civil"
+          className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72"
+        >
+          DIPLOMADOS EN INGENIERÍA CIVIL
         </div>
-          <div id='graduateAmbientaly' className='mb-20 lg:mb-10'>
-            <DiploCivil />
-          </div>
-          <div id='ambiental' className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72">
-            DIPLOMADOS EN INGENIERÍA AMBIENTAL
+        <div id="graduateAmbientaly" className="mb-20 lg:mb-10">
+          <DiploCivil />
         </div>
-          <div className='mb-20 lg:mb-10'>
-            <DiploAmbiental />
-          </div>
-          <div id='alimentarias' className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72">
-            DIPLOMADOS EN INGENIERÍA AGRÓNOMA
+        <div
+          id="ambiental"
+          className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72"
+        >
+          DIPLOMADOS EN INGENIERÍA AMBIENTAL
         </div>
-          <div className='mb-20 lg:mb-10'>
-            <DiploAlimentarias />
-          </div>
-          <div id='agronoma' className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72">
-            DIPLOMADOS EN INGENIERÍA ALIMENTOS
+        <div className="mb-20 lg:mb-10">
+          <DiploAmbiental />
         </div>
-          <div className='mb-20 lg:mb-10'>
-            <DiploAgronoma />
-          </div>
-        <div id='derecho' className="ml-15flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72">
-            DIPLOMADOS EN DERECHO
+        <div
+          id="alimentarias"
+          className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72"
+        >
+          DIPLOMADOS EN INGENIERÍA ALIMENTOS
         </div>
-          <div className='mb-20 lg:mb-10'>
-            <DiploDerecho />
-          </div>
-          
+        <div className="mb-20 lg:mb-10">
+          <DiploAlimentarias />
+        </div>
+        <div
+          id="agronoma"
+          className="flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72"
+        >
+          DIPLOMADOS EN INGENIERÍA AGRÓNOMA
+        </div>
+        <div className="mb-20 lg:mb-10">
+          <DiploAgronoma />
+        </div>
+        <div
+          id="derecho"
+          className="ml-15flex text-primaryblue text-center text-3xl  lg:text-5xl xl:text-5xl lg:w-3/4 mb-4 font-bold font-poppins lg:ml-72"
+        >
+          DIPLOMADOS EN DERECHO
+        </div>
+        <div className="mb-20 lg:mb-10">
+          <DiploDerecho />
+        </div>
       </div>
-{/*       <Whatsapp />
- */}    </section>
-  )
-}
+      {/*       <Whatsapp />
+       */}{" "}
+    </section>
+  );
+};
 
 export default Graduate;
